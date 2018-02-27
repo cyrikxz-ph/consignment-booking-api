ba7a3ecdf47fc53b81a247da39b432e1c583ce72
Aleson Llanes <cyrikxz.ph@gmail.com>
2018-02-27 21:01:40 +0800

    - fix seed-suburb

--- server/scripts/seed-suburbs.js
+++ server/scripts/seed-suburbs.js
@@ -1,4 +1,5 @@
 'use strict';
+const Promise = require('bluebird');
 const util = require('util');
 const fs = require('fs');
 const path = require('path');
@@ -8,12 +9,14 @@ const Suburb = app.models.suburb;
 const PostCode = app.models.postCode;
 const State = app.models.state;
 
-const stat = util.promisify(fs.read);
 const seedData = fs.readFileSync(
   path.join(__dirname, '../seed-data/post_codes.csv'),
-  'utf8');
+  'utf8'
+);
 
-Promise.all(seedData.split('\n').map(function(line) {
+let newSuburbsArray = [];
+const subs = seedData.split('\n').map(function(line, idx) {
+  console.log(idx);
   if (line) {
     const row = line.split(',');
 
@@ -22,26 +25,44 @@ Promise.all(seedData.split('\n').map(function(line) {
     const newState = row[2];
     let stateId, postCodeId;
 
-    console.log(newSuburb, newPostCode, newState);
-    State.findOrCreate({'code': newState})
+    return State.findOrCreate({'code': newState})
       .then(function(state) {
-        stateId = state[0].id;
-        return PostCode.findOrCreate({code: newPostCode});
+        if (state) {
+          stateId = state[0].id;
+          return PostCode.findOrCreate({code: newPostCode});
+        } else {
+          console.log(newState);
+          return Promise.reject();
+        }
       })
       .then(function(postCode) {
         postCodeId = postCode[0].id;
-        return Suburb.create({
+        newSuburbsArray.push({
+          name: newSuburb,
+          postCodeId: postCodeId,
+          stateId: stateId,
+        });
+        return Promise.resolve({
           name: newSuburb,
           postCodeId: postCodeId,
           stateId: stateId,
         });
       })
       .catch(function(err) {
-        console.log(err);
         return Promise.reject(err);
       });
+  } else {
+    Promise.resolve();
   }
-}))
+});
+
+Promise.all(subs).then(function(ret) {
+  // console.log(newSuburbsArray);
+  return Suburb.create(newSuburbsArray);
+})
+.then(function() {
+  console.log('Completed!');
+})
 .catch(function(e) {
   console.log(e);
 });
